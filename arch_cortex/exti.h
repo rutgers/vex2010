3e4d159a7e805e547dd01b44469b0161888bce44
Cody Schafer <cpschafer@gmail.com>
2011-02-18 03:25:45 -0500

    remove non comp2010

--- arch_cortex/exti.h
+++
@@ -1,6 +0,0 @@
-#ifndef EXTI_H_
-#define EXTI_H_
-
-void exti_init(void);
-
-#endif
--- arch_pic/build.mk
+++
@@ -1,8 +0,0 @@
-SOURCE  += $(ARCHDIR)/hax.c        \
-           $(ARCHDIR)/ifi_lib.c
-HEADERS += $(ARCHDIR)/master.h     \
-           $(ARCHDIR)/ifi_lib.h
-
-ARCH_EXT = hex
-COMPILER = sdcc
-include $(ARCHDIR)/$(COMPILER).mk
